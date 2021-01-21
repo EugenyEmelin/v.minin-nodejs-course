152be2c9ec1ff2f461850d76f9aa71e75a52473f
EugenyEmelin <emelin.eug@ya.ru>
2021-01-21 18:12:21 +0300

    changed npm start script

--- utils/keys/index.js
+++ utils/keys/index.js
@@ -1,5 +1,9 @@
+console.log(process.env.NODE_ENV, 'NODE_ENV')
+
 if (process.env.NODE_ENV === 'production') {
+  console.log('production mode')
   module.exports = require('./keys.prod')
 } else {
+  console.log('dev mode')
   module.exports = require('./keys.dev')
 }
